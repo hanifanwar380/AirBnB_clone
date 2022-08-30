58c8232cf5bf25fc9c5c0692f6dc58809d8bcda4
oolufolabii <otitoola.olufolabi@gmail.com>
2022-08-30 12:12:02 +0100

    Base Model

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,21 @@
+
+#!/usr/bin/python3
+"""Defines unittests for models/base_model.py.
+
+
+Unittest classes:
+    TestBaseModel_instantiation
+    TestBaseModel_save
+    TestBaseModel_to_dict
+    """
+
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.base_model import BaseModel
+
+
+if __name__ == "__main__":
+    unittest.main()
