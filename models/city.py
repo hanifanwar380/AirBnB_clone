8f209dc47ed99f66fa7a402956fd75e5de8a3dca
hanifanwar380 <hanifanwar380@gmail.com>
2022-08-30 17:21:42 +0300

    Added City Model

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Project HBNB City Module """
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """The city class comprises of state ID and name"""
+    state_id = ""
+    name = ""
+    
