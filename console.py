64422f415052cd1e57e92cb606990467e46636c7
oolufolabii <otitoola.olufolabi@gmail.com>
2022-08-31 10:52:18 +0100

    AirBnB Console

--- console.py
+++ console.py
@@ -24,6 +24,32 @@ class HBNBCommand(cmd.Cmd):
         print("")
         return True
 
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel,
+            saves it (to the JSON file)"""
+        pass
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance
+            based on the class name and id"""
+        pass
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and
+            id (save the change into the JSON file)."""
+        pass
+
+    def all(self, arg):
+        """Prints all string representation of all
+            instances based or not on the class name"""
+        pass
+
+    def update(self, arg):
+        """Updates an instance based on the class name and id
+            by adding or updating attribute (save the change
+            into the JSON file)"""
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
