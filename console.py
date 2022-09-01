f172d3a2baae305d420ada4f3df96a0c0092743a
oolufolabii <otitoola.olufolabi@gmail.com>
2022-09-01 12:22:51 +0100

    console

--- console.py
+++ console.py
@@ -63,10 +63,10 @@ class HBNBCommand(cmd.Cmd):
     def do_destroy(self, arg):
         """Deletes an instance based on the class name and
             id (save the change into the JSON file)."""
-        
+
         if arg == "" or arg is None:
             print("** class name missing **")
-        
+
         else:
             words = arg.split(" ")
             if words[0] not in storage.classes():
@@ -81,31 +81,80 @@ class HBNBCommand(cmd.Cmd):
                     del storage.all()[key]
                     storage.save()
 
-    def all(self, arg):
+    def do_all(self, arg):
         """Prints all string representation of all
             instances based or not on the class name"""
-        
+
         if arg != "":
             words = arg.split(' ')
-        
+
             if words[0] not in storage.classes():
                 print("** class doesn't exist **")
             else:
                 line = [str(obj) for key, obj in storage.all().items()
-                     if type(obj).__name__ == words[0]]
+                        if type(obj).__name__ == words[0]]
                 print(line)
         else:
             line = [str(obj) for key, obj in storage.all().items()]
             print(line)
 
-    def update(self, arg):
+    def do_update(self, arg):
         """Updates an instance based on the class name and id
             by adding or updating attribute (save the change
             into the JSON file)"""
-        
+
         if arg == "" or arg is None:
             print("** class name missing **")
             return
+
+        regx = r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
+
+        match = re.search(regx, arg)
+
+        classname = match.group(1)
+        uid = match.group(2)
+        attribute = match.group(3)
+        value = match.group(4)
+
+        if not match:
+            print("** class name missing **")
+        elif classname not in storage.classes():
+            print("** class doesn't exist **")
+        elif uid is None:
+            print("** instance id missing **")
+
+        else:
+            key = "{}.{}".format(classname, uid)
+            if key not in storage.all():
+                print("** no instance found **")
+            elif not attribute:
+                print("** attribute name missing **")
+            elif not value:
+                print("** value missing **")
+
+            else:
+                type_cast = None
+                if not re.search('^".*"$', value):
+                    if '.' in value:
+                        type_cast = float
+                    else:
+                        type_cast = int
+
+                else:
+                    value = value.replace('"', '')
+
+                attributes = storage.attributes()[classname]
+
+                if attribute in attributes:
+                    value = attributes[attribute](value)
+
+                elif type_cast:
+                    try:
+                        value = type_cast(value)
+                    except ValueError:
+                        pass  # fine, stay a string then
+                setattr(storage.all()[key], attribute, value)
+                storage.all()[key].save()
 
 
 if __name__ == '__main__':
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,8 @@
     """
 
 import json
+import os
+import datetime
 from models.base_model import BaseModel
 
 
@@ -63,6 +65,8 @@ class FileStorage:
 
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
 
         try:
             with open(FileStorage.__file_path) as file:
@@ -73,3 +77,40 @@ class FileStorage:
                     self.new(eval(class_name)(**obj))
         except FileNotFoundError:
             return
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname."""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
